cb23d9cc62e8140135da9cbe0bd60257b734003b
bergLowe <sumer2000singh@hotmail.com>
2021-04-04 22:03:06 +0530

    Api_key file changes

--- src/utils/api_key.js
+++ src/utils/api_key.js
@@ -1,6 +1,8 @@
-// Key for OpenWeatherMap
-w_key = "a8d8be9aef57f46eda28fa64f4045ec5"
-// Key for MapBox
-m_key = "pk.eyJ1IjoicmVtdXMyMDAwIiwiYSI6ImNrbWhvazg3bTAzeG8ycGxuZnd0MHVsZ2oifQ.haBnB3oRKmXfHsPfu3turQ"
+var keys = {
+    // Key for OpenWeatherMap
+    w_key: "a8d8be9aef57f46eda28fa64f4045ec5",
+    // Key for MapBox
+    m_key: "pk.eyJ1IjoicmVtdXMyMDAwIiwiYSI6ImNrbWhvazg3bTAzeG8ycGxuZnd0MHVsZ2oifQ.haBnB3oRKmXfHsPfu3turQ",
+};
 
-module.exports = {w_key, m_key}
+module.exports = keys
