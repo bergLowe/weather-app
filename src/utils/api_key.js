d9ff541827bdaae892d21c9a5476f8c06573dcf3
bergLowe <sumer2000singh@hotmail.com>
2021-04-27 17:38:32 +0530

    Deleted env var

--- src/utils/api_key.js
+++
@@ -1,8 +0,0 @@
-var keys = {
-    // Key for OpenWeatherMap
-    w_key: "a8d8be9aef57f46eda28fa64f4045ec5",
-    // Key for MapBox
-    m_key: "pk.eyJ1IjoicmVtdXMyMDAwIiwiYSI6ImNrbWhvazg3bTAzeG8ycGxuZnd0MHVsZ2oifQ.haBnB3oRKmXfHsPfu3turQ",
-};
-
-module.exports = keys
